99a0aa62a434a52183fb06baefcd59ca0b52bd46
Jeff Yutzler <jyutzler@yahoo.com>
2017-01-13 11:57:43 -0500

    updating archive to get right stuff into Nexus

--- ci/archive.sh
+++ ci/archive.sh
@@ -20,14 +20,8 @@ gometalinter --install
 
 go get -v github.com/venicegeo/bf-ia-broker
 
-cd $GOPATH/src/github.com/venicegeo/bf-ia-broker
-
-# run unit tests w/ coverage collection
-go test -v -coverprofile=planet.cov github.com/venicegeo/bf-ia-broker/planet
-go test -v -coverprofile=tides.cov github.com/venicegeo/bf-ia-broker/tides
-cp ./planet.cov $root/planet.cov
-cp ./tides.cov $root/tides.cov
-
+# Planet package
+cd $GOPATH/src/github.com/venicegeo/bf-ia-broker/planet
 
 # lint
 gometalinter \
@@ -43,8 +37,61 @@ gometalinter \
 --exclude="Url.* should be .*URL" \
 --exclude="[iI][dD] can be fmt\.Stringer" \
 --exclude=" duplicate of [A-Za-z\._0-9]*" \
-./... | tee $root/lint.txt
-wc -l $root/lint.txt
+./... | tee $root/planet-lint.txt
+wc -l $root/planet-lint.txt
+
+# run unit tests w/ coverage collection
+go test -v -coverprofile=planet.cov github.com/venicegeo/bf-ia-broker/planet
+cp ./planet.cov $root/planet.cov
+
+# Tides package
+cd $GOPATH/src/github.com/venicegeo/bf-ia-broker/tides
+
+# lint
+gometalinter \
+--deadline=60s \
+--concurrency=6 \
+--vendor \
+--exclude="exported (var)|(method)|(const)|(type)|(function) [A-Za-z\.0-9]* should have comment" \
+--exclude="comment on exported function [A-Za-z\.0-9]* should be of the form" \
+--exclude="Api.* should be .*API" \
+--exclude="Http.* should be .*HTTP" \
+--exclude="Id.* should be .*ID" \
+--exclude="Json.* should be .*JSON" \
+--exclude="Url.* should be .*URL" \
+--exclude="[iI][dD] can be fmt\.Stringer" \
+--exclude=" duplicate of [A-Za-z\._0-9]*" \
+./... | tee $root/tides-lint.txt
+wc -l $root/tides-lint.txt
+
+# run unit tests w/ coverage collection
+go test -v -coverprofile=tides.cov github.com/venicegeo/bf-ia-broker/tides
+cp ./tides.cov $root/tides.cov
+
+# Util package
+cd $GOPATH/src/github.com/venicegeo/bf-ia-broker/util
+
+# lint
+gometalinter \
+--deadline=60s \
+--concurrency=6 \
+--vendor \
+--exclude="exported (var)|(method)|(const)|(type)|(function) [A-Za-z\.0-9]* should have comment" \
+--exclude="comment on exported function [A-Za-z\.0-9]* should be of the form" \
+--exclude="Api.* should be .*API" \
+--exclude="Http.* should be .*HTTP" \
+--exclude="Id.* should be .*ID" \
+--exclude="Json.* should be .*JSON" \
+--exclude="Url.* should be .*URL" \
+--exclude="[iI][dD] can be fmt\.Stringer" \
+--exclude=" duplicate of [A-Za-z\._0-9]*" \
+./... | tee $root/util-lint.txt
+wc -l $root/util-lint.txt
+
+# run unit tests w/ coverage collection
+go test -v -coverprofile=util.cov github.com/venicegeo/bf-ia-broker/util
+cp ./util.cov $root/util.cov
+
 
 # gather some data about the repo
 
@@ -53,6 +100,9 @@ cp $GOPATH/bin/$APP ./$APP.bin
 tar cvzf $APP.$EXT \
     $APP.bin \
     planet.cov \
-	tides.cov \
-    lint.txt
+    planet-lint.txt \
+    tides.cov \
+    tides-lint.txt \
+    util.cov \
+    util-lint.txt \
 tar tzf $APP.$EXT
